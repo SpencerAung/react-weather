d7f3383a21942769caf36d4443720c30e2ce3712
susuaung <susuaung.ms@gmail.com>
2017-03-25 00:58:54 +0900

    API call

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,13 +1,36 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var openWeatherMap = require('OpenWeatherMap');
 var Weather = React.createClass({
+  getInitialState: function() {
+    return {
+      location: "Seoul",
+      temp: 20
+    };
+  },
+  handleSearch: function(location) {
+    var that = this;
+    openWeatherMap.getTemp(location).then(function(temp) {
+      that.setState({
+        location: location,
+        temp: temp
+      });
+    }, function (errorMessage) {
+      alert(errorMessage);
+    });
+    // this.setState({
+    //   location: location,
+    //   temp: 27
+    // });
+  },
   render: function() {
+    var { temp, location } = this.state;
     return (
       <div>
         <h3>Get Weather</h3>
-        <WeatherForm />
-        <WeatherMessage />
+        <WeatherForm onSearch={this.handleSearch}/>
+        <WeatherMessage temp={temp} location={location} />
       </div>
 
     );
